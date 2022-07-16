1203740d47f0eaee892b3b4249bf40d4db28503d
Catherine Kim <ikptlsmile@gmail.com>
2022-07-16 17:47:34 +0900

    2.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+//css loader --> style
+import '../css/index.css'
+import '../sass/index.scss'
 console.log('Welcome')
 
+
+
 //git remote add origin https://github.com/catherinek07/webpack-temple.git
+//git add .
+//git commit -m "name"
+//git push origin master
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
 //common.js
 //module path, plugin
 const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
     //start
     entry : './js/main.js',
     output : {
-    path: path.resolve(__dirname, 'public'),
-    filename: ' main.js',
+    // path: path.resolve(__dirname, 'disk'),
+    // filename: ' main.js',
     clean: true
-    }
+    },
+    module:{
+        rules:[
+            {test:/\.s?css$/,
+            use:['style-loader',
+                'css-loader',
+                'sass-loader'
+            ]}
+        ]
+    },
+    plugins:[
+        new HtmlPlugin(
+            {template:'index.html'}
+        ),
+        new CopyPlugin(
+            {patterns:[
+                {from:'static'}
+                ]}
+        )
+    ]
+    // ,devServer:{
+    //     host:'localhost',
+    //     port:8080,
+    //     hot:true
+    // }
 }
